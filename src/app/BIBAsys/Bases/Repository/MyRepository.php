73ab94c05b5d891991397520fa58fbbb26e7f12a
mercury <bibaryskoutepov@mail.ru>
2023-09-30 13:50:31 +0600

    Added MyRepository

---
+++ src/app/BIBAsys/Bases/Repository/MyRepository.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\BIBAsys\Bases\Repository;
+
+use Illuminate\Database\Query\Builder;
+use Illuminate\Support\Collection;
+
+class MyRepository
+{
+    protected string $tableName;
+    protected string $tableAlias = 'main';
+    protected string $tableKey = 'id';
+    protected ?Builder $query = null;
+
+    public function __construct()
+    {
+        if (method_exists($this, 'useMain')) {
+            $this->useMain();
+        }
+    }
+    public function tableName(): string
+    {
+        return $this->tableName;
+    }
+
+    public function tableAlias(): string
+    {
+        return $this->tableAlias;
+    }
+
+    public function tableAliasDot(): string
+    {
+        return $this->tableAlias() . '.';
+    }
+
+    public function query(): Builder
+    {
+        return $this->query;
+    }
+
+    public function whereKey(mixed $id): static
+    {
+        $this->query()
+            ->where($this->tableAliasDot() . $this->tableKey(), '=', $id);
+
+        return $this;
+    }
+
+    public function get(): Collection
+    {
+        return $this->query
+            ->get();
+    }
+
+    public function first(): object|null
+    {
+        return $this
+            ->query()
+            ->first();
+    }
+}
